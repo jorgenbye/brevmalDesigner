c6110b0c3ce12479891876f455ec2d83405f237c
jorgenbye <noreply>
2015-03-18 14:33:05 +0000

    Add component ref-field

---
+++ ui/ref-field.reel/ref-field.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/ref-field.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class RefField
+ * @extends Component
+ */
+exports.RefField = Component.specialize(/** @lends RefField# */ {
+    constructor: {
+        value: function RefField() {
+            this.super();
+        }
+    }
+});
