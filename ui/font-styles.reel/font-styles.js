c3f97c9360d93c863ccaffd8d493e230f373ea71
jorgenbye <noreply>
2015-03-19 11:36:46 +0000

    Add component FontStyles

---
+++ ui/font-styles.reel/font-styles.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/font-styles.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class FontStyles
+ * @extends Component
+ */
+exports.FontStyles = Component.specialize(/** @lends FontStyles# */ {
+    constructor: {
+        value: function FontStyles() {
+            this.super();
+        }
+    }
+});
