046e308a1cca24876540e6d050fb94363b07e92f
jorgenbye <noreply>
2015-03-19 12:01:48 +0000

    Add component sidebar-right

---
+++ ui/sidebars/sidebar-right.reel/sidebar-right.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/sidebars/sidebar-right.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class SidebarRight
+ * @extends Component
+ */
+exports.SidebarRight = Component.specialize(/** @lends SidebarRight# */ {
+    constructor: {
+        value: function SidebarRight() {
+            this.super();
+        }
+    }
+});
