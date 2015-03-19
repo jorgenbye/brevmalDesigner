a5d223a1d3478265bb46c6ce33e4b412ffe011e6
jorgenbye <noreply>
2015-03-19 11:37:11 +0000

    Add component heading

---
+++ ui/font-styles.reel/heading.reel/heading.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/font-styles.reel/heading.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Heading
+ * @extends Component
+ */
+exports.Heading = Component.specialize(/** @lends Heading# */ {
+    constructor: {
+        value: function Heading() {
+            this.super();
+        }
+    }
+});
