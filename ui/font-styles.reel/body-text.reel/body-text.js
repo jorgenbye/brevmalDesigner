6de4c9217660658148bbdeb4e00a0916eb9a9e7d
jorgenbye <noreply>
2015-03-19 11:37:21 +0000

    Add component body-text

---
+++ ui/font-styles.reel/body-text.reel/body-text.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/font-styles.reel/body-text.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class BodyText
+ * @extends Component
+ */
+exports.BodyText = Component.specialize(/** @lends BodyText# */ {
+    constructor: {
+        value: function BodyText() {
+            this.super();
+        }
+    }
+});
