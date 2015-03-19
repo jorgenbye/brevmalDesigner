551f7e6313123cb008f21760d27f9843e65a6a1b
jorgenbye <noreply>
2015-03-19 10:07:18 +0000

    Add component headertable

---
+++ ui/headertable.reel/headertable.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/headertable.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Headertable
+ * @extends Component
+ */
+exports.Headertable = Component.specialize(/** @lends Headertable# */ {
+    constructor: {
+        value: function Headertable() {
+            this.super();
+        }
+    }
+});
