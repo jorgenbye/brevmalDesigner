6d2a4b46c35138d7bb607509ba57720d8786fe41
jorgenbye <noreply>
2015-03-19 12:01:38 +0000

    Add component sidebar-left

---
+++ ui/sidebars/sidebar-left.reel/sidebar-left.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/sidebars/sidebar-left.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class SidebarLeft
+ * @extends Component
+ */
+exports.SidebarLeft = Component.specialize(/** @lends SidebarLeft# */ {
+    constructor: {
+        value: function SidebarLeft() {
+            this.super();
+        }
+    }
+});
